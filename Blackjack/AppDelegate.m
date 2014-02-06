123b72c37c04348d81e368698f3ac6afd6203c31
Brian <bhw2113@columbia.edu>
2014-02-05 18:35:40 -0800

    added afnetworking and lumberjack and configured logging

--- Blackjack/AppDelegate.m
+++ Blackjack/AppDelegate.m
@@ -7,12 +7,23 @@
 //
 
 #import "AppDelegate.h"
+#import "DDASLLogger.h"
+#import "DDTTYLogger.h"
+#import "DDFileLogger.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    // Adding cocoalumberjack loggers
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    // Configuring colors
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor greenColor] backgroundColor:nil forFlag:LOG_FLAG_INFO];
+    
     return YES;
 }
 							
